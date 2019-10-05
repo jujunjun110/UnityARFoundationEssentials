e5b385e6f3391897b0f9ad77754d813f541c1bd1
Jun Ito <jun_ito@voyagegroup.com>
2019-10-05 15:22:57 +0900

    Update: Shaderに外部からパラメータ渡し（かっこいい映像とれた）

--- Assets/Scripts/PostEffect.cs
+++ Assets/Scripts/PostEffect.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
-public class PostEffect : MonoBehaviour
-{
+public class PostEffect : MonoBehaviour {
     public Material _material;
-    void OnRenderImage(RenderTexture src, RenderTexture dest)
-    {
+    private float size;
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        size = Mathf.Sin(Time.time) * 0.5f + 0.5f;
+        Debug.LogFormat("Size: {0}", size);
+        _material.SetFloat("_Size", size);
+
         Graphics.Blit(src, dest, _material);
     }
 }
