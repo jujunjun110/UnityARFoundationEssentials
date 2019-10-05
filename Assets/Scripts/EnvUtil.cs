f41536bb475be8669bbf20730295d56b5f26d84c
Jun Ito <jun_ito@voyagegroup.com>
2019-10-05 14:30:30 +0900

    Add: Automatic camera changer

---
+++ Assets/Scripts/EnvUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvUtil : MonoBehaviour {
+    [SerializeField] Camera productionCamera;
+    [SerializeField] Camera debugCamera;
+
+    void Start() {
+        Debug.LogFormat("IS EDITOR?: {0}", Application.isEditor);
+
+        if (Application.isEditor) {
+            productionCamera.gameObject.SetActive(false);
+            debugCamera.gameObject.SetActive(true);
+        } else {
+            productionCamera.gameObject.SetActive(true);
+            debugCamera.gameObject.SetActive(false);
+        }
+    }
+}
