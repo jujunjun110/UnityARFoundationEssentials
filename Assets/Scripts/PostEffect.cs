d9bb2d15d414091db4d55245239e7d271b18447b
Jun Ito <jun_ito@voyagegroup.com>
2019-10-05 13:50:04 +0900

    Update: ポストエフェクトできるところまで

--- Assets/Scripts/PostEffect.cs
+++ Assets/Scripts/PostEffect.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class PostEffect : MonoBehaviour
 {
-    public Material monoTone;
+    public Material _material;
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        Graphics.Blit(src, dest, monoTone);
+        Graphics.Blit(src, dest, _material);
     }
 }
